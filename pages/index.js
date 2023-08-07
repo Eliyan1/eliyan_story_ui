6980986b60d95c3471d28f38899e0d698bd2f739
Hans Hendrikse <hans7hendrikse@hotmail.com>
2023-08-08 00:30:09 +0200

    Made a front-end mockup of how the app is supposed to look.

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,42 @@
 import React from'react';
-import Head from 'next/head'
 import Link from'next/link';
 import Flexstyle from '../styles/flexbox.module.css'
-import Image from 'next/image';
 import UIcomp from '../styles/ui_components.module.css'
+import CharButton from '../components/charbutton'
+import StoryContent from '../components/storycontent'
+import CharSaveLoad from '../components/charsaveload'
 	
 export default class extends React.Component {
 	render() {
 		return (
 		<>
 		<div className={`${Flexstyle.container}`}>
-			<div className={`${Flexstyle.headeritem}`}><Link href="/story">Story</Link></div>
-			<div className={`${Flexstyle.headeritem}`}><Link href="/story">Audio</Link></div>
-			<div className={`${Flexstyle.headeritem}`}><Link href="/story">Visual</Link></div>
+      		<div className={`${Flexstyle.storybox}`}>
+				<StoryContent/>
+
+				<div className={`${Flexstyle.storysaveload}`}>
+					<div className={`${UIcomp.storysave}`}>Save</div>
+					<div className={`${UIcomp.storyload}`}>Load</div>
+				</div>
+			</div>
+			<div className={`${Flexstyle.characterwrap}`}>
+				<div className={`${Flexstyle.characterbox}`}>
+					<CharButton char={{name:'Grumgar'}} />
+					<CharButton char={{name:'Lunynn'}} />
+					<CharButton char={{name:'Raythor'}} />
+					<CharButton char={{name:'Luke v.d. Bolt'}} />
+					<CharButton char={{name:'Gary'}} />
+				</div>
+				<div> 
+					<CharSaveLoad/>				
+				</div>
+			</div>
+
 		</div>
 		<div className={`${Flexstyle.container}`}>
-      		<div className={`${Flexstyle.storybox}`}><Image href="../image/Stone_Story_Frame.png"/></div>
+			<div className={`${Flexstyle.headeritem}`}><Link href="/story">Story</Link></div>
+			<div className={`${Flexstyle.headeritem}`}><Link href="/audio">Audio</Link></div>
+			<div className={`${Flexstyle.headeritem}`}><Link href="/visual">Visual</Link></div>
 		</div>
 		</>
 		)
--- pages/story.js
+++ pages/story.js
@@ -1,4 +1,40 @@
-export default function FirstPost() {
-    return <h1>First Post</h1>;
-  }
-  
+import React from'react';
+import Link from'next/link';
+import Flexstyle from '../styles/flexbox.module.css'
+import UIcomp from '../styles/ui_components.module.css'
+import CharButton from '../components/charbutton'
+import StoryContent from '../components/storycontent'
+import CharSaveLoad from '../components/charsaveload'
+	
+export default function Story()  {
+	return <>
+		<div className={`${Flexstyle.container}`}>
+      		<div className={`${Flexstyle.storybox}`}>
+				<StoryContent/>
+
+				<div className={`${Flexstyle.storysaveload}`}>
+					<div className={`${UIcomp.storysave}`}>Save</div>
+					<div className={`${UIcomp.storyload}`}>Load</div>
+				</div>
+			</div>
+			<div className={`${Flexstyle.characterwrap}`}>
+				<div className={`${Flexstyle.characterbox}`}>
+					<CharButton char={{name:'Grumgar'}} />
+					<CharButton char={{name:'Lunynn'}} />
+					<CharButton char={{name:'Raythor'}} />
+					<CharButton char={{name:'Luke v.d. Bolt'}} />
+					<CharButton char={{name:'Gary'}} />
+				</div>
+				<div> 
+					<CharSaveLoad/>				
+				</div>
+			</div>
+
+		</div>
+		<div className={`${Flexstyle.container}`}>
+			<div className={`${Flexstyle.headeritem}`}><Link href="/story">Story</Link></div>
+			<div className={`${Flexstyle.headeritem}`}><Link href="/audio">Audio</Link></div>
+			<div className={`${Flexstyle.headeritem}`}><Link href="/visual">Visual</Link></div>
+		</div>
+	</>
+}
