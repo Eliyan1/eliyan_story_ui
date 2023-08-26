895a1f6e91ef18f904570ee9ff5e9b4fcca73430
Hans Hendrikse <hans7hendrikse@hotmail.com>
2023-08-26 15:52:14 +0200

    added mongoose dependancy, removed index layout as it is a copy of /story

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,5 @@
 import React from'react';
-import Link from'next/link';
-import Flexstyle from '../styles/flexbox.module.css'
-import StoryStyle from '../styles/story.module.css'
-import CharButton from '../components/charbutton'
-import StoryContent from '../components/storycontent'
 import Footer from '../components/footer'
-import Story from './story'
 	
 export default class extends React.Component {
 	render() {
