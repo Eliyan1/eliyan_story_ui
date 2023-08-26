7a9f929e05bc90fea41716d2b0af5d8b0761c8e2
Hans Hendrikse <hans7hendrikse@hotmail.com>
2023-08-26 15:36:05 +0200

    removed index, as this was the same as story

--- pages/index.js
+++ pages/index.js
@@ -5,46 +5,12 @@ import StoryStyle from '../styles/story.module.css'
 import CharButton from '../components/charbutton'
 import StoryContent from '../components/storycontent'
 import Footer from '../components/footer'
+import Story from './story'
 	
 export default class extends React.Component {
 	render() {
 		return (
-		<>
-		<div className={`${Flexstyle.container}`}>
-      		<div className={`${Flexstyle.storybox}`}>
-				<StoryContent/>
-
-				<div className={`${Flexstyle.storysaveload}`}>
-					<div className={`${StoryStyle.storysave}`}>Save</div>
-					<div className={`${StoryStyle.storyload}`}>Load</div>
-				</div>
-			</div>
-			<div className={`${Flexstyle.characterwrap}`}>
-				<div className={`${Flexstyle.characterbox}`}>
-					<CharButton char={{name:'Grumgar'}} />
-					<CharButton char={{name:'Lunynn'}} />
-					<CharButton char={{name:'Raythor'}} />
-					<CharButton char={{name:'Luke v.d. Bolt'}} />
-					<CharButton char={{name:'Gary'}} />
-				</div>
-				<div> 
-					<div className={`${StoryStyle.charactersavecolumn}`}>
-      					<div className={`${StoryStyle.charactersaverow}`}>
-        					<div className={`${StoryStyle.charactersavebutton}`}>Create Character</div>
-        					<div className={`${StoryStyle.charactersavebutton}`}>Roll Initiative</div>
-      					</div>
-						<div className={`${StoryStyle.charactersaverow}`}>
-							<div className={`${StoryStyle.charactersavebutton}`}>Save Characters</div>
-							<div className={`${StoryStyle.charactersavebutton}`}>Load Characters</div>
-						</div>
-					</div>			
-				</div>
-			</div>
-
-		</div>
-		
-		<Footer/>
-		</>
+		<Footer/>	
 		)
 	}
 }
